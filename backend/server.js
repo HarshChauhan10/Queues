69ea11d77efb87054c7063a319d0a1ca319d1a4c
Harsh Chauhan <harshchauhan2867@gmail.com>
2025-01-24 23:28:06 +0530

    initial

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+
+// Middlewares
+app.use(express.json());
+
+//cors
+//we can acess the backend form any IP
+
+app.use(cors());
+
+
+// API endpoints
+app.get('/', (req, res) => {
+    res.send("API WORKING");
+});
+
+// Start express server
+app.listen(port, () => console.log('Server started on PORT: ' + port));
